15efd43109ad7ed200f5bcfcb6c3ffd89c6ab9fa
Ed FitzGerald <edward.fitzgerald@fetch.ai>
2020-02-26 10:43:30 +0000

    Add simple validation and help to script

--- scripts/submit-bulk-tx.sh
+++ scripts/submit-bulk-tx.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
+# check the number of arguments
+if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
+  echo "Usage: $0 <BINARY TX FILE>" >&2
+  exit 1
+fi
+
+# run CURL
 curl  -H "Content-Type: application/vnd.fetch-ai.transaction+bulk" --data-binary @$1 http://127.0.0.1:8000/api/contract/submit
